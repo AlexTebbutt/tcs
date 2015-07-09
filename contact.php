76300ce827b2f1c750093017fedbf39becaf79eb
Alex Tebbutt <alex.tebbutt@digi-nut.co.uk>
2015-07-09 11:14:19 +0100

    Working on Contact page

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
 		'activePage' => 'contact',
 	));
 	
-	perch_content('Contact Hero');
+	perch_content('Contact Main Copy');
 	perch_content('Contact Inlink Panels');
 	perch_layout('global.footer', array(
 		'activePage' => 'contact',
