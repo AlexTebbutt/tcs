832d96872aa36667812e6dd20cdea7c35aa593be
Alex Tebbutt <alex.tebbutt@digi-nut.co.uk>
2015-09-23 16:11:20 +0100

    Bespoke page build

--- bespoke.php
+++ bespoke.php
@@ -19,26 +19,18 @@
 	
 </div>
 	  
-<div class="row">
-
-  <div class="twelve columns inset-white">
 		  
-		  <!-- Bespoke albums show here -->
-		  <?php 
-			  	
-			  perch_gallery_albums(array(
-				  										'template'=>'bespoke_album.html',
-															'filter'=>'category',
-															'match'=>'eq',
-															'value'=>'bespoke' 
-														));
-			?>
-		  
-	</div>
+  <!-- Bespoke albums show here -->
+  <?php 
+	  	
+	  perch_gallery_albums(array(
+		  										'template'=>'bespoke_album.html',
+													'filter'=>'category',
+													'match'=>'eq',
+													'value'=>'bespoke' 
+												));
+	?>
 	
-</div>
-
-
 
 <?php
 	perch_layout('global.footer', array(
